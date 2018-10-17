c2b5a95a393f0f144976a6f9fcbf03760b6ce96e
Anthony Nguyen Cong Trang <anctrang@ensc.fr>
2018-10-17 12:06:04 +0200

    debut projet

---
+++ Pluscourtchemin/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Pluscourtchemin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelDepart = new System.Windows.Forms.Label();
+            this.labelArrivee = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBoxgraphe = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxO = new System.Windows.Forms.TextBox();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBoxRep = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonNouvelleEtape = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(638, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Init1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(478, 476);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(478, 548);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "6";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(530, 504);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "A*";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelDepart
+            // 
+            this.labelDepart.AutoSize = true;
+            this.labelDepart.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDepart.Location = new System.Drawing.Point(536, 9);
+            this.labelDepart.Name = "labelDepart";
+            this.labelDepart.Size = new System.Drawing.Size(69, 21);
+            this.labelDepart.TabIndex = 4;
+            this.labelDepart.Text = "Départ";
+            this.labelDepart.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelArrivee
+            // 
+            this.labelArrivee.AutoSize = true;
+            this.labelArrivee.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelArrivee.Location = new System.Drawing.Point(661, 9);
+            this.labelArrivee.Name = "labelArrivee";
+            this.labelArrivee.Size = new System.Drawing.Size(74, 21);
+            this.labelArrivee.TabIndex = 5;
+            this.labelArrivee.Text = "Arrivée";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(607, 350);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(128, 121);
+            this.listBox1.TabIndex = 6;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(583, 117);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(152, 180);
+            this.treeView1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(607, 572);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Init2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBoxgraphe
+            // 
+            this.listBoxgraphe.FormattingEnabled = true;
+            this.listBoxgraphe.Location = new System.Drawing.Point(518, 208);
+            this.listBoxgraphe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxgraphe.Name = "listBoxgraphe";
+            this.listBoxgraphe.Size = new System.Drawing.Size(100, 121);
+            this.listBoxgraphe.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(665, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(59, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 260);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(407, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Faites tourner l\'algorithme de Dijkstra et remplir les textbox si dessous étape p" +
+    "ar étape";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxO
+            // 
+            this.textBoxO.Location = new System.Drawing.Point(116, 491);
+            this.textBoxO.Name = "textBoxO";
+            this.textBoxO.Size = new System.Drawing.Size(100, 20);
+            this.textBoxO.TabIndex = 14;
+            this.textBoxO.Text = "{}";
+            this.textBoxO.TextChanged += new System.EventHandler(this.textBoxO_TextChanged);
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(260, 491);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(100, 20);
+            this.textBoxF.TabIndex = 15;
+            this.textBoxF.Text = "{}";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 494);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "O";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(241, 494);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "F";
+            // 
+            // listBoxRep
+            // 
+            this.listBoxRep.FormattingEnabled = true;
+            this.listBoxRep.Location = new System.Drawing.Point(116, 364);
+            this.listBoxRep.Name = "listBoxRep";
+            this.listBoxRep.Size = new System.Drawing.Size(244, 121);
+            this.listBoxRep.TabIndex = 18;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(177, 548);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 20);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Valider exercice";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // buttonNouvelleEtape
+            // 
+            this.buttonNouvelleEtape.Location = new System.Drawing.Point(366, 484);
+            this.buttonNouvelleEtape.Name = "buttonNouvelleEtape";
+            this.buttonNouvelleEtape.Size = new System.Drawing.Size(91, 33);
+            this.buttonNouvelleEtape.TabIndex = 20;
+            this.buttonNouvelleEtape.Text = "Nouvelle étape";
+            this.buttonNouvelleEtape.UseVisualStyleBackColor = true;
+            this.buttonNouvelleEtape.Click += new System.EventHandler(this.buttonNouvelleEtape_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 640);
+            this.Controls.Add(this.buttonNouvelleEtape);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBoxRep);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxO);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxgraphe);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelArrivee);
+            this.Controls.Add(this.labelDepart);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelDepart;
+        private System.Windows.Forms.Label labelArrivee;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBoxgraphe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBoxRep;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonNouvelleEtape;
+        public System.Windows.Forms.TextBox textBoxO;
+        public System.Windows.Forms.TextBox textBoxF;
+    }
+}
+
---
+++ Pluscourtchemin/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Pluscourtchemin
+{
+    public partial class Form1 : Form
+    {
+        public List<String>[] reponses;
+        public string noeudsFermes;
+        public int etape;
+        static public double[,] matrice;
+        static public int nbnodes = 10;
+        static public int numinitial;
+        static public int numfinal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            etape = 0;
+            reponses = new List<String>[2];
+            reponses[0] = new List<string>();
+            reponses[1] = new List<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            matrice = new double[nbnodes, nbnodes];
+            for (int i = 0; i < nbnodes; i++)
+                for (int j = 0; j < nbnodes; j++)
+                    matrice[i, j] = -1;
+
+            matrice[0, 1] = 3; matrice[1, 0] = 3;
+            matrice[0, 2] = 5; matrice[2, 0] = 5;
+            matrice[0, 3] = 7; matrice[3, 0] = 7;
+            matrice[1, 4] = 8; matrice[4, 1] = 8;
+            matrice[2, 4] = 3; matrice[4, 2] = 3;
+            matrice[4, 5] = 7; matrice[5, 4] = 7;
+            matrice[5, 6] = 4; matrice[6, 5] = 4;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numinitial = Convert.ToInt32(textBox1.Text);
+            numfinal = Convert.ToInt32(textBox2.Text);
+            SearchTree g = new SearchTree();
+            Node2 N0 = new Node2();
+            N0.numero = numinitial;
+            List<GenericNode> solution = g.RechercheSolutionAEtoile(N0);
+
+            Node2 N1 = N0;
+            for (int i = 1; i < solution.Count; i++)
+            {
+                Node2 N2 = (Node2)solution[i];
+                listBox1.Items.Add(Convert.ToString(N1.numero)
+                     + "--->" + Convert.ToString(N2.numero)
+                     + "   : " + Convert.ToString(matrice[N1.numero, N2.numero]));
+                N1 = N2;
+            }
+
+            g.GetSearchTree(treeView1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            StreamReader monStreamReader = new StreamReader("graphe1.txt");
+
+            // Lecture du fichier avec un while, évidemment !
+            // 1ère ligne : "nombre de noeuds du graphe
+            string ligne = monStreamReader.ReadLine();
+            int i = 0;
+            while (ligne[i] != ':') i++;
+            string strnbnoeuds = "";
+            i++; // On dépasse le ":"
+            while (ligne[i] == ' ') i++; // on saute les blancs éventuels
+            while (i < ligne.Length)
+            {
+                strnbnoeuds = strnbnoeuds + ligne[i];
+                i++;
+            }
+            nbnodes = Convert.ToInt32(strnbnoeuds);
+
+            matrice = new double[nbnodes, nbnodes];
+            for (i = 0; i < nbnodes; i++)
+                for (int j = 0; j < nbnodes; j++)
+                    matrice[i, j] = -1;
+
+            // Ensuite on a ls tructure suivante : 
+            //  arc : n°noeud départ    n°noeud arrivée  valeur
+            //  exemple 4 : 
+            ligne = monStreamReader.ReadLine();
+            while (ligne != null)
+            {
+                i = 0;
+                while (ligne[i] != ':') i++;
+                i++; // on passe le :
+                while (ligne[i] == ' ') i++; // on saute les blancs éventuels
+                string strN1 = "";
+                while (ligne[i] != ' ')
+                {
+                    strN1 = strN1 + ligne[i];
+                    i++;
+                }
+                int N1 = Convert.ToInt32(strN1);
+
+                // On saute les blancs éventuels
+                while (ligne[i] == ' ') i++;
+                string strN2 = "";
+                while (ligne[i] != ' ')
+                {
+                    strN2 = strN2 + ligne[i];
+                    i++;
+                }
+                int N2 = Convert.ToInt32(strN2);
+
+                // On saute les blancs éventuels
+                while (ligne[i] == ' ') i++;
+                string strVal = "";
+                while ((i < ligne.Length) && (ligne[i] != ' '))
+                {
+                    strVal = strVal + ligne[i];
+                    i++;
+                }
+                double val = Convert.ToDouble(strVal);
+
+                matrice[N1, N2] = val;
+                matrice[N2, N1] = val;
+                listBoxgraphe.Items.Add(Convert.ToString(N1)
+                   + "--->" + Convert.ToString(N2)
+                   + "   : " + Convert.ToString(matrice[N1, N2]));
+
+                ligne = monStreamReader.ReadLine();
+            }
+            // Fermeture du StreamReader (obligatoire) 
+            monStreamReader.Close();
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public bool verifierEntreeTextBox()
+        {
+            bool acceptable = true;
+            int cpt = 0;
+            int cpt2 = 0;
+
+            /*while (this.textBoxO.Text[cpt] != '}')
+            {
+                if(this.textBoxO.Text[cpt] ==' ')
+                {
+                   cpt2 = cpt;
+                   while(cpt2< this.textBoxO.Text.Count() - 2) 
+                    
+                    {
+                        this.textBoxO.Text[cpt2] = this.textBoxO.Text[cpt2 + 1];
+                        cpt2++;
+                    }
+                    this.textBoxO.Text[]='':
+                }
+                else
+                {
+                    cpt++;
+                }
+
+            }*/
+
+
+            if (this.textBoxO.Text[0] != '{' || this.textBoxF.Text[0] != '{' || this.textBoxO.Text[textBoxO.Text.Count() - 1] != '}' ||
+                this.textBoxF.Text[textBoxF.Text.Count() - 1] != '}') // On teste si les premiers et derniers caractères des textbox sont bien des accolades
+            {
+                acceptable = false;
+            }
+
+        
+            for (int i = 1; i < textBoxO.Text.Count(); i = 1 + 2)
+            {
+                if (this.textBoxO.Text[i] < 65 || this.textBoxO.Text[i] > 90 && this.textBoxO.Text[i] < 97 || this.textBoxO.Text[i] > 122)
+                // On vérifie si tous les caractères impairs sont bien des lettres (gestion ascii + maj/min)
+                {
+                    acceptable = false;
+                }
+            }
+
+            return acceptable;
+        }
+
+        private void buttonNouvelleEtape_Click(object sender, EventArgs e)
+        {
+
+            if (verifierEntreeTextBox())
+            {
+                reponses[0].Add(this.textBoxO.Text);
+                reponses[1].Add(this.textBoxF.Text);
+                this.listBoxRep.Items.Add("F =" + reponses[0][etape] + "\t O=" + reponses[1][etape]);
+                etape++;
+            }
+
+
+        }
+
+        private void textBoxO_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
